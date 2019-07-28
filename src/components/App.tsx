542820ab63bc1ad8ec0dd00d8277c9f5612ebb20
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2019-07-28 11:06:13 +0900

    init for calculator

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,50 +1,44 @@
 import React, { FC } from 'react';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Icon from '@material-ui/core/Icon';
-import Typography from '@material-ui/core/Typography';
+// import { makeStyles, createStyles } from '@material-ui/core/styles';
+// import Button from '@material-ui/core/Button';
+// import Card from '@material-ui/core/Card';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
+// import Icon from '@material-ui/core/Icon';
+// import Typography from '@material-ui/core/Typography';
 
-import './App.css';
-
-interface AppProps {
-  timeLeft: number;
-  reset: () => void;
-}
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    card: {
-      maxWidth: 275,
-    },
-  }),
-);
-
-const AppComponent: FC<AppProps> = ({ timeLeft, reset }) => {
-  const classes = useStyles();
-
+const AppComponent: FC<{}> = () => {
   return (
-    <>
-      <div className="container">
-        <header>
-          <h1>タイマー</h1>
-        </header>
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography variant="h5" component="h2">time</Typography>
-            <Typography variant="h5" component="h2">{timeLeft}</Typography>
-          </CardContent>
-          <CardActions>
-            <Button color="primary" onClick={reset}>
-              <Icon>redo</Icon>
-              Reset
-            </Button>
-          </CardActions>
-        </Card>
-      </div>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>演算子</th>
+          <th>数字</th>
+          <th>結果</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>
+            <form>
+              <label htmlFor={"plus"}>
+                +
+              </label>
+            </form>
+          </td>
+          <td>
+            <form>
+              <label htmlFor={"plus"}>
+                +
+              </label>
+            </form>
+          </td>
+          <td>result</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,31 +1,8 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import AppComponent from '../components/App';
 
-const useTimer = (limitSec: number): [number, () => void] => {
-  const [timeLeft, setTimeLeft] = useState(limitSec);
-
-  const reset = () => {
-    setTimeLeft(limitSec);
-  };
-
-  const tick = () => {
-    setTimeLeft(prevTime => (prevTime === 0 ? limitSec : prevTime - 1));
-  };
-
-  useEffect(() => {
-    const timerId = setInterval(tick, 1000);
-
-    return () => clearInterval(timerId);
-  }, []);
-
-  return [timeLeft, reset];
-}
-
 const AppContainer: FC = () => {
-  const LIMIT = 60;
-  const [timeLeft, reset] = useTimer(LIMIT);
-
-  return <AppComponent timeLeft={timeLeft} reset={reset} />;
+  return <AppComponent />;
 };
 
 export default AppContainer;

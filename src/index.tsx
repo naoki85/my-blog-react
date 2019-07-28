3f911e78760ab96c15dbd9c442d34b0ef1fd5510
naoki85 <naoki.yoneyama.85@gmail.com>
2019-07-29 07:52:26 +0900

    Init reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
 import App from './containers/App';
+import reducer from "./reducers";
+import { compose, createStore, StoreEnhancerStoreCreator } from "redux";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export interface CustomWindow extends Window {
+  __REDUX_DEVTOOLS_EXTENSION__: () => StoreEnhancerStoreCreator<{}, {}>;
+}
+declare let window: CustomWindow;
+const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION__() as typeof compose) || compose;
+
+const store = createStore(reducer, composeEnhancers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

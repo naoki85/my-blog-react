3d106e57fef279b1643052bd20186717efcea476
naoki85 <naoki.yoneyama.85@gmail.com>
2019-08-08 20:27:56 +0900

    Set post properties in component

--- src/components/posts/Show.tsx
+++ src/components/posts/Show.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react';
+import Typography from "@material-ui/core/Typography";
+import {createStyles, makeStyles, Theme} from "@material-ui/core";
 
 export interface Post {
   Id: number;
@@ -14,9 +16,29 @@ interface PostShowStateProps {
 
 export interface PostProps extends PostShowStateProps {}
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    postArea: {
+      marginTop: theme.spacing(3),
+    },
+  })
+);
+
 const PostShowComponent: FC<PostProps> = (props) => {
+  const classes = useStyles();
+
   return (
-    <h2>{ `postId: ${props.post.Id} の記事だよ` }</h2>
+    <div className={classes.postArea}>
+      <Typography variant="h4">
+        {props.post.Title}
+      </Typography>
+      <Typography variant="subtitle1" color="textSecondary">
+        {props.post.PublishedAt}
+      </Typography>
+      <Typography paragraph>
+        {props.post.Content}
+      </Typography>
+    </div>
   );
 };
 

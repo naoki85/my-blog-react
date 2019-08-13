f3d3fbf4bda2a327a8c12babefbadddb7cac1dd4
naoki85 <naoki.yoneyama.85@gmail.com>
2019-08-13 23:53:07 +0900

    Install React Helmet

--- src/components/posts/Show.tsx
+++ src/components/posts/Show.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import Helmet from 'react-helmet';
 import Typography from "@material-ui/core/Typography";
 import {createStyles, makeStyles, Theme} from "@material-ui/core";
 import {convertToHtml} from "../../utils/Markdown";
@@ -31,30 +32,46 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const PostShowComponent: FC<PostProps> = (props) => {
   const classes = useStyles();
+  const getDescription = () => {
+    const content = props.post.Content as string;
+
+    return content.substr(0, 160);
+  };
 
   return (
-    <div className={classes.postArea}>
-      <Typography variant="h4">
-        {props.post.Title}
-      </Typography>
-      <Typography variant="subtitle1" color="textSecondary">
-        {props.post.PublishedAt}
-      </Typography>
-      <TweetButton
-        id={props.post.Id}
-        title={props.post.Title}
-      />
-      <HatebuButton
-        id={props.post.Id}
-        title={props.post.Title}
-      />
-      <Typography
-        paragraph
-        className={'preview-area'}
-        dangerouslySetInnerHTML={{__html: convertToHtml(props.post.Content)}}
-      />
-      <RecommendedBooksComponent recommendedBooks={props.recommendedBooks}/>
-    </div>
+    <>
+      <Helmet>
+        <meta name={'description'} content={getDescription()} />
+        <meta name={'og:url'} content={`https://blog.naoki85.me/posts/${props.post.Id}`} />
+        <meta name={'og:title'} content={props.post.Title} />
+        <meta name={'og:description'} content={getDescription()} />
+        <meta name={'og:image'} content={props.post.ImageUrl} />
+        <meta name={'twitter:card'} content={'summary'} />
+      </Helmet>
+
+      <div className={classes.postArea}>
+        <Typography variant="h4">
+          {props.post.Title}
+        </Typography>
+        <Typography variant="subtitle1" color="textSecondary">
+          {props.post.PublishedAt}
+        </Typography>
+        <TweetButton
+          id={props.post.Id}
+          title={props.post.Title}
+        />
+        <HatebuButton
+          id={props.post.Id}
+          title={props.post.Title}
+        />
+        <Typography
+          paragraph
+          className={'preview-area'}
+          dangerouslySetInnerHTML={{__html: convertToHtml(props.post.Content)}}
+        />
+        <RecommendedBooksComponent recommendedBooks={props.recommendedBooks}/>
+      </div>
+    </>
   );
 };
 

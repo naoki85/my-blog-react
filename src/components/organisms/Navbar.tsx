f9384c44b2d0878c7d55854d2893283f00802afa
naoki85 <naoki.yoneyama.85@gmail.com>
2020-02-24 10:46:58 +0900

    Revert "Revert sidemenu styles"
    This reverts commit 585090ec5a2be7048f63ffc9f556da41e886f769.

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -72,10 +72,7 @@ const useStyles = makeStyles(theme => ({
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
+    width: 0,
   },
   paper: {
     padding: theme.spacing(2),
